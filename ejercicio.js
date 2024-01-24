9c7e47ba35ff3502ec67014e27bbb634d52e0f37
Cerebrus <dr2208384@gmail.com>
2024-01-24 19:45:08 -0300

    ejercicio de find

---
+++ ejercicio.js
@@ -0,0 +1,8 @@
+function miNombre (nombre){
+    console.log(`Mi nombre es ${nombre}`);
+}
+miNombre ("Benito Rodriguez");
+
+const ListaDeCompres = ["leche", "pan", "huevo", "queso", "fideos"];
+const ListaDeCompresNecesarias = lista.find ((lista)=> lista === "queso");
+console.log(ListaDeCompresNecesarias);
