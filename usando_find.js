bb4d5937f49841ada716d3460f3a764195d4f06e
Cerebrus <dr2208384@gmail.com>
2024-01-24 18:54:19 -0300

    ejemplos primarios de funciones para find

---
+++ usando_find.js
@@ -0,0 +1,16 @@
+//definiendo la funcion 
+//funciones normales
+function decirHola(){
+    //bueno, aqui dentro vas a 
+    // escribir las sentencias de tu progama o
+    // algoritmo
+    console.log("Hola");
+}
+
+function otraFuncion(){
+    alert("soy una alerta");
+}
+
+//invoco, llamo, call
+decirHola();
+otraFuncion();
