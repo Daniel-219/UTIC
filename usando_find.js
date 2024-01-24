e6ae0c5f99719d93705faee14f468158411f3a6c
Cerebrus <dr2208384@gmail.com>
2024-01-24 19:20:20 -0300

    viendo otros tipos de funciones

--- usando_find.js
+++ usando_find.js
@@ -11,6 +11,27 @@ function otraFuncion(){
     alert("soy una alerta");
 }
 
+//lo que esta adentro del parentesis so los parametros, y es por defecto
+function decirNombre(nombre="no puso en nombre"){
+    console.log(`El nombre ingresado es ${nombre}`);
+}
+function dameElNombreDeMiMascotaNormal (nombre){
+    return nombre;
+}
+
+//funcion flecha, la flechita es el retorno
+const dameElNombreDeMiMascota = (nombre)=> nombre;
+
+
+//funciones anonimas
+const recibiendoFuncionAnonima = function(){console("soy una funcion anonima")};
+
+
 //invoco, llamo, call
 decirHola();
 otraFuncion();
+recibiendoFuncionAnonima();
+//lo que esta dentro del parentesis se llama argumento
+decirNombre("Abigails");
+const nombreDeMascota = dameElNombreDeMiMascota("firulais");
+ 
