d5147c009bb5442f87a49841a6acf4d401fa98a1
Cerebrus <dr2208384@gmail.com>
2024-01-25 18:59:27 -0300

    ejercicio 3.3

---
+++ ejercicio_3.3.js
@@ -0,0 +1,8 @@
+// Crear un array con los valores 1, 2, 3
+let arrayOriginal = [1, 2, 3];
+
+// Anidar el array original en un nuevo array, tres veces
+let arrayAnidado = [arrayOriginal, arrayOriginal, arrayOriginal];
+
+// Mostrar en pantalla el valor 2 de uno de los arrays en la consola
+console.log(arrayAnidado[0][1]); // Esto imprimirá '2' en la consola
